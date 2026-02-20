b2a9c4309109ab626b6042ca889f94e5193fd51e
Uchit Kumar <88498054+uchitkumar34@users.noreply.github.com>
2026-02-20 13:23:22 +0530

    Comment out existing code in remove.js
    Comment out all code in remove.js for future reference.

--- remove.js
+++ remove.js
@@ -1,17 +1,17 @@
-const arr = [1, 2, 4, 5, 6, 2, 3, 5, 1];
-//first method
-const newArray = arr.reduce((acc, curr,) => {
-    if (!acc.includes(curr)) {
-        acc.push(curr);
-    }
-    return acc;
-}, []);
-//second method
-const newArr = arr.filter((item, index) => {
-    return arr.indexOf(item) === index;
-})
-//set method 
-const newAr = [...new Set(arr)]
+// const arr = [1, 2, 4, 5, 6, 2, 3, 5, 1];
+// //first method
+// const newArray = arr.reduce((acc, curr,) => {
+//     if (!acc.includes(curr)) {
+//         acc.push(curr);
+//     }
+//     return acc;
+// }, []);
+// //second method
+// const newArr = arr.filter((item, index) => {
+//     return arr.indexOf(item) === index;
+// })
+// //set method 
+// const newAr = [...new Set(arr)]
 
 
-console.log(newAr.join(' '));
+// console.log(newAr.join(' '));
