338460021bcedd397318f66b87cb08cb847f90f3
Uchit Kumar <88498054+uchitkumar34@users.noreply.github.com>
2026-02-20 13:23:40 +0530

    Comment out duplicate removal logic in removeDuplicate.js
    Comment out the original implementation of duplicate removal and the filter approach.

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,17 +1,17 @@
-const arr = [1, 2, 4, 5, 6, 2, 3, 5];
-// first approach
+// const arr = [1, 2, 4, 5, 6, 2, 3, 5];
+// // first approach
 
-const obj = {};
-const originalArr = []; 
-for (let i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
-    if (!obj[arr[i]]) {
-        obj[arr[i]] = true
-        originalArr.push(arr[i]);
-    }
-}
-console.log(obj);
-console.log(originalArr);
+// const obj = {};
+// const originalArr = []; 
+// for (let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+//     if (!obj[arr[i]]) {
+//         obj[arr[i]] = true
+//         originalArr.push(arr[i]);
+//     }
+// }
+// console.log(obj);
+// console.log(originalArr);
 
-//second by filter
-// const newArr=arr.filter((item,index)=>)
+// //second by filter
+// // const newArr=arr.filter((item,index)=>)
